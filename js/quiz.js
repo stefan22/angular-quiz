366ab564c8959228a3f2b7e43ef5620ecf80d84a
Stefan Lissia <stefan.emailacct@gmail.com>
2015-10-06 18:53:37 +0100

    loading json data into app

--- js/quiz.js
+++ js/quiz.js
@@ -4,6 +4,19 @@
 
 	app.controller('QuizController',['$scope','$http','$sce', function($scope,$http,$sce) {
 
+		$scope.score = 0;
+		$scope.activeQuestion = -1;
+		$scope.activeQuestionAnswered = 0;
+		$scope.percentage = 0;
+
+		$http.get('quiz_data.json').then(function(quizData){
+			$scope.myQuestions = quizData.data; //all data inside myQuestions
+			$scope.totalQuestions = $scope.myQuestions.length;
+
+
+		});
+
+
 
 	}]);
 	
